b6c751fe13d59bc1adf9ee776104ad558751c4a6
Rodrigo Werlich <rodrigo.werlich@gmail.com>
2018-01-24 15:03:44 -0200

    alteração na verificação de usuario logado

--- vendor/rwerlich/php-classes/src/Model/Repository/RepositoryUser.php
+++ vendor/rwerlich/php-classes/src/Model/Repository/RepositoryUser.php
@@ -9,27 +9,22 @@ class RepositoryUser {
     private $bd;
 
     public function __construct() {
-        $this->bd = new \PDO('mysql:host='.HOST.';dbname='.DBNAME.'', DBUSER, PASS);
+        $this->bd = new \PDO('mysql:host=' . HOST . ';dbname=' . DBNAME . '', DBUSER, PASS);
     }
-    
+
     public function getFromSession() {
         $user = new User();
-        if (isset($_SESSION['user'])) {
-            $user = $_SESSION['user'];            
-        } 
+        if ($_SESSION['user']->getIduser() > 0 && $_SESSION['user']->getAdmin() == 0) {
+            $user = $_SESSION['user'];
+        }
         return $user;
     }
-    
+
     public function checkLogin() {
-        if (isset($_SESSION['user'])) {
-            $user = $_SESSION['user'];
-            if ($user->getAdmin() == 0) {
-                header("Location: /ecommerce/admin/login");
-                exit;
-            }
-        } else {
-            return false;
-        }
+        if (!isset($_SESSION['user']) || $_SESSION['user'] == NULL || !$_SESSION['user']->getIduser() > 0) {
+            header("Location: /ecommerce/admin/login");            
+            exit;
+        } 
     }
 
     public function login(string $login, string $password) {
@@ -46,34 +41,31 @@ class RepositoryUser {
         }
     }
 
-    public function verifyLogin() {
-        if (isset($_SESSION['user'])) {
-            $user = $_SESSION['user'];
-            if ($user->getAdmin() == 0) {
-                header("Location: /ecommerce/admin/login");
-                exit;
-            }
-        } else {
+    public function verifyLogin() {        
+        if (!isset($_SESSION['user']) || $_SESSION['user'] == NULL) {
+            header("Location: /ecommerce/admin/login");            
+            exit;
+        } else if ($_SESSION['user']->getIduser() > 0 && $_SESSION['user']->getAdmin() == 0) {
             header("Location: /ecommerce/admin/login");
             exit;
         }
     }
-    
+
     public function listAll() {
         $query = "SELECT * FROM tb_users ORDER BY iduser DESC";
         $stmt = $this->bd->prepare($query);
         $stmt->execute();
         return $stmt->fetchAll(\PDO::FETCH_ASSOC);
     }
-    
+
     public function find(int $iduser) {
         $query = "SELECT * FROM tb_users WHERE iduser = :iduser";
         $stmt = $this->bd->prepare($query);
         $stmt->bindValue(':iduser', $iduser);
         $stmt->execute();
-        return $stmt->fetch(\PDO::FETCH_ASSOC);        
+        return $stmt->fetch(\PDO::FETCH_ASSOC);
     }
-    
+
     public function update(User $user) {
         $query = "UPDATE tb_users SET
                     login = :login,
@@ -94,7 +86,6 @@ class RepositoryUser {
         $stmt->execute();
     }
 
-
     public function insert(User $user) {
         $query = "INSERT INTO tb_users (login, password, admin, nome, email, phone) "
                 . "VALUES (:login, :password, :admin, :nome, :email, :phone)";
@@ -108,7 +99,6 @@ class RepositoryUser {
         $stmt->execute();
     }
 
-    
     public function delete(int $iduser) {
         $query = "DELETE FROM tb_users WHERE iduser = :iduser";
         $stmt = $this->bd->prepare($query);
