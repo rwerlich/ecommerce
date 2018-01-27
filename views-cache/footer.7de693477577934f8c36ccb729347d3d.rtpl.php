82e8e6fb47a5d8bf8aa7fbc1fc3773798c820ad4
Rodrigo Werlich <rodrigo.werlich@gmail.com>
2018-01-27 00:52:15 -0200

    removido arquivos desnecessarios

--- views-cache/footer.7de693477577934f8c36ccb729347d3d.rtpl.php
+++ views-cache/footer.7de693477577934f8c36ccb729347d3d.rtpl.php
@@ -4,12 +4,12 @@
             <div class="row">
                 <div class="col-md-3 col-sm-6">
                     <div class="footer-about-us">
-                        <h2>Hcode Store</h2>
+                        <h2>Werlich Ecommerce</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis sunt id doloribus vero quam laborum quas alias dolores blanditiis iusto consequatur, modi aliquid eveniet eligendi iure eaque ipsam iste, pariatur omnis sint! Suscipit, debitis, quisquam. Laborum commodi veritatis magni at?</p>
                         <div class="footer-social">
-                            <a href="https://www.facebook.com/hcodebr" target="_blank"><i class="fa fa-facebook"></i></a>
-                            <a href="https://twitter.com/hcodebr" target="_blank"><i class="fa fa-twitter"></i></a>
-                            <a href="https://www.youtube.com/channel/UCjWENuSH2gX55-y7QSZiWxA" target="_blank"><i class="fa fa-youtube"></i></a>
+                            <a href="#" target="_blank"><i class="fa fa-facebook"></i></a>
+                            <a href="#" target="_blank"><i class="fa fa-twitter"></i></a>
+                            <a href="#" target="_blank"><i class="fa fa-youtube"></i></a>
                         </div>
                     </div>
                 </div>
@@ -55,7 +55,7 @@
             <div class="row">
                 <div class="col-md-8">
                     <div class="copyright">
-                        <p>&copy; <script>document.write(new Date().getFullYear())</script> Rodrigo Werlich. <a href="http://werlich.blog.br" target="_blank">werlich.blog.br</a></p>
+                        <p>&copy; <script>document.write(new Date().getFullYear())</script> Rodrigo Werlich - <a href="http://werlich.blog.br" target="_blank">werlich.blog.br</a></p>
                     </div>
                 </div>
                 
