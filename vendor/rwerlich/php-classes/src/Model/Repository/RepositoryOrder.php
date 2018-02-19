3dae09018ee5424950c142abbe281d11720b1798
Rodrigo Werlich <rodrigo.werlich@gmail.com>
2018-02-18 23:32:55 -0300

    admin paginação e busca pedido

--- vendor/rwerlich/php-classes/src/Model/Repository/RepositoryOrder.php
+++ vendor/rwerlich/php-classes/src/Model/Repository/RepositoryOrder.php
@@ -78,6 +78,58 @@ class RepositoryOrder implements SessionMsgs{
         $stmt->execute();
         return $stmt->fetchAll(\PDO::FETCH_ASSOC);
     }
+    
+    public function getPage(int $page = 1, int $itens = 10) {        
+        $start = ($page -1) * $itens;
+        $query = "SELECT a.idorder, b.status, c.vltotal, c.vlfreight, d.nome 
+                FROM tb_orders AS a
+                INNER JOIN tb_ordersstatus AS b ON b.idstatus = a.idstatus
+                INNER JOIN tb_carts AS c ON c.idcart = a.idcart
+                INNER JOIN tb_users AS d ON d.iduser = a.iduser
+                ORDER BY a.idorder DESC LIMIT {$start}, {$itens}";
+        $stmt = $this->bd->prepare($query);
+        $stmt->execute();
+        $results = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        
+        $query = "SELECT FOUND_ROWS() as total";
+        $stmt = $this->bd->prepare($query);
+        $stmt->execute();
+        $total = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        
+        return [
+            'data' => $results,
+            'total' => $total[0]['total'],
+            'pages' => ceil($total[0]['total'] / $itens)
+        ];
+    }
+    
+    public function getPageSearch(string $search, int $page = 1, int $itens = 10) {        
+        $start = ($page -1) * $itens;
+        
+        $query = "SELECT a.idorder, b.status, c.vltotal, c.vlfreight, d.nome 
+                FROM tb_orders AS a
+                INNER JOIN tb_ordersstatus AS b ON b.idstatus = a.idstatus
+                INNER JOIN tb_carts AS c ON c.idcart = a.idcart
+                INNER JOIN tb_users AS d ON d.iduser = a.iduser
+                WHERE a.idorder = :id OR d.nome LIKE :search OR b.status LIKE :search
+                ORDER BY a.idorder DESC LIMIT {$start}, {$itens}";
+        $stmt = $this->bd->prepare($query);
+        $stmt->bindValue(':search', "%{$search}%");
+        $stmt->bindValue(':id', $search);
+        $stmt->execute();
+        $results = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        
+        $query = "SELECT FOUND_ROWS() as total";
+        $stmt = $this->bd->prepare($query);
+        $stmt->execute();
+        $total = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        
+        return [
+            'data' => $results,
+            'total' => $total[0]['total'],
+            'pages' => ceil($total[0]['total'] / $itens)
+        ];
+    }
 
     public static function setMsgError($msg) {
         $_SESSION[RepositoryOrder::SESSION_ERROR] = $msg;
